edca040f5780a2be160e55ea9cf0349ed30c6325
chayale <c0556728176@gmail.com>
2024-05-13 15:41:53 +0200

    continue user controller and user service

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # This is a sample Python script.
+
 import uvicorn
+from bson import ObjectId
 from fastapi import FastAPI
 
 from controllers.userController import User_Router
+from dataAccess.dataAccess import db
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -10,10 +13,11 @@ from controllers.userController import User_Router
 
 app = FastAPI()
 
-app.include_router(User_Router, prefix='/Tasks', tags=["Tasks"])
+app.include_router(User_Router, prefix='/Users', tags=["Users"])
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8080)
+     uvicorn.run("main:app", host="127.0.0.1", port=8080)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
